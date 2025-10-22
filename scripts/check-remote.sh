52de674cf17e0f8da5b889677bf9fc0ad6c37824
Samuel Boyden <sam@stboyden.com>
2025-10-22 21:17:59 +0100

    update: state of config at 2025-10-22 21:17:59

--- scripts/check-remote.sh
+++ scripts/check-remote.sh
@@ -3,20 +3,21 @@
 git remote update 2>&1 >/dev/null
 
 status=$(git -P status -uno | head -n2 | tail -n1)
+tag="[GIT CHECK REMOTE]"
 
 case "$status" in
 	*"up to date"*)
-	    echo "[GIT CHECK REMOTE] All up to date."
+	    echo "$tag All up to date."
 		exit 0
 		;;
 
 	*"behind"*)
-        echo "[GIT CHECK REMOTE] We're behind remote."
+        echo "$tag We're behind remote."
 	    exit 1
 		;;
 
 	*"ahead"*)
-	    echo "[GIT CHECK REMOTE] We're ahead of the remote."
+	    echo "$tag We're ahead of the remote."
 	    exit 2
 		;;
 
