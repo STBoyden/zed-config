14507989f2d0b363d7f37e1b237a02ea009c070a
Samuel Boyden <sam@stboyden.com>
2026-05-08 19:00:02 +0100

    update: state of config at 2026-05-08 19:00:02
    Updates from device with hostname: Sams-MacBook-Pro-13.local

--- scripts/check-remote.sh
+++ scripts/check-remote.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 
+tag="[GIT CHECK REMOTE]"
+
+current_remote=$(git remote get-url origin)
+
+new_origin="ssh://git@codeberg.org/STBoyden/zed-config.git"
+if [ "$current_remote" != "$new_origin" ]; then
+    echo "$tag Remote is not set to Codeberg - migrating..."
+    git remote set-url origin "$new_origin" || git remote add origin "$new_origin"
+    git fetch || exit 1
+else
+    echo "$tag Remote is already set to Codeberg - skipping migration."
+fi
+
 git remote update 2>&1 >/dev/null
 
 status=$(git -P status -uno | head -n2 | tail -n1)
-tag="[GIT CHECK REMOTE]"
 
 case "$status" in
 	*"up to date"*)
