dd3217a593120b14613287d11f5d3494662d72c9
Samuel Boyden <sam@stboyden.com>
2026-05-20 15:44:48 +0100

    update: state of config at 2026-05-20 15:44:48
    Updates from device with hostname: Sams-MacBook-Pro-13.local

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -4,9 +4,5 @@ echo "Installing bun..."
 
 curl -fsSL https://bun.sh/install | bash
 
-echo -e "\nInitialising git repository in ./zed-ultraViolet..."
-
-(cd ./zed-ultraViolet && git init)
-
 echo -e "\nInstalling fonts..."
 ./scripts/install-fonts.sh
