ae2bba2d59893ff9961c4bb2ec48e0ff31067bb6
Samuel Boyden <sboyden@certosoftware.com>
2026-05-20 15:36:01 +0100

    update: state of config at 2026-05-20 15:36:01
    Updates from device with hostname: Sams-Mac-mini-M4.local

--- scripts/install-fonts.sh
+++ scripts/install-fonts.sh
@@ -8,8 +8,8 @@ function darwin() {
 		exit 1
 	fi
 
-	brew install --cask font-monaspace-nf
-	echo "Monaspace fonts installed"
+	brew install --cask font-monaspace-nf font-fira-code-nerd-font
+	echo "Monaspace & Fira Code Nerd Fonts installed"
 }
 
 function linux() {
@@ -18,10 +18,19 @@ function linux() {
 		exit 1
 	fi
 
+	if ! command -v unzip &>/dev/null; then
+		echo "unzip is not installed"
+		exit 1
+	fi
+
 	mkdir ./tmp
 
 	echo "Creating ~/.local/share/fonts if it doesn't already exist"
 	mkdir -p ~/.local/share/fonts
+
+	echo "Downloading Fira Code Nerd Font"
+	curl -L -o ~/.local/share/fonts/FiraCode.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/FiraCode.zip
+	(cd ~/.local/share/fonts && unzip FiraCode.zip && rm FiraCode.zip)
 
 	echo "Cloning Monaspace from GitHub"
 	git clone git@github.com:githubnext/monaspace ./tmp/monaspace
